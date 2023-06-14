506688c349ba869d1fbaf24b54daaf388cd5f7be
VladBogda <vladbogdantsev@gmail.com>
2023-06-14 14:32:07 +0300

    exercise.repository (+impl interface )

---
+++ Sport-diary-backend/src/main/java/sport/diary/api/exercises/model/Exercise.java
@@ -0,0 +1,21 @@
+package sport.diary.api.exercises.model;
+
+public class Exercise {
+
+    private String title;
+
+    public Exercise() {
+    }
+
+    public Exercise(String title) {
+        this.title = title;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+}
